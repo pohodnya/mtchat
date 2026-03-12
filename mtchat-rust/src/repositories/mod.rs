8d712ca9985b948fabe78ae75d87e620979becd5
Aleksei Pokhodnia <pohodnyaa@gmail.com>
2026-03-12 16:20:57 +0100

    refactor(backend): remove unused Repository trait

--- mtchat-rust/src/repositories/mod.rs
+++ mtchat-rust/src/repositories/mod.rs
@@ -13,10 +13,3 @@ pub use dialog_repo::DialogRepository;
 pub use message_repo::MessageRepository;
 pub use participant_repo::ParticipantRepository;
 pub use scope_repo::AccessScopeRepository;
-
-use sqlx::PgPool;
-
-/// Trait for repositories that need database access
-pub trait Repository {
-    fn pool(&self) -> &PgPool;
-}
